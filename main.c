971b16e9fecda7281c5316fc4c256f5b1aefb38d
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2022-01-12 16:59:56 +0100

    Add some verbose printf to test.

--- main.c
+++ main.c
@@ -21,7 +21,7 @@
 
 #ifdef _MSC_VER
 #include <Windows.h>
-#define sleep(X) Sleep(X)
+#define sleep(X) Sleep(X * 1000)
 #else
 #include <unistd.h>
 #endif
@@ -34,51 +34,68 @@ int main(int argc, char *argv[])
 	char *v_char_1 = create_cl_char_p ("vchar_1");
 	double v_double_1 = create_cl_double ("vdouble_1");
 
+	printf("# Read mandatory args: %d %s %lf\n", v_int_1, v_char_1, v_double_1);
 	assert(v_int_1 == atoi(argv[1]));
 	assert(strcmp(v_char_1, argv[2]) == 0);
 	assert(v_double_1 == atof(argv[3]));
 
 	int o_int_1 = create_optional_cl_int ("oint_1", -1);
+	printf("# Read: o_int_1 %d\n", o_int_1);
 	assert(o_int_1 == ((argc > 4 ? atoi(argv[4]) : -1)));
 
 	double o_double_1 = create_optional_cl_double ("odouble_1", 0.5);
+	printf("# Read: o_double_1 %lf\n", o_double_1);
 	assert(o_double_1 == ((argc > 5 ? atof(argv[5]) : 0.5)));
 
 	char *o_char_1 = create_optional_cl_char_p ("ochar_p_1", NULL);
+	printf("# Read: o_char_1 %s\n", o_char_1);
 	assert((argc > 6 ? strcmp(o_char_1, argv[6]) == 0 : o_char_1 == NULL));
 
 	char *o_char_2 = create_optional_cl_char_p ("ochar_p_2", "");
+	printf("# Read: o_char_2 %s\n", o_char_2);
 	assert(strcmp(o_char_2, ((argc > 7 ? argv[7] : ""))) == 0);
 
 	char *o_char_3 = create_optional_cl_char_p ("ochar_p_3", "opt1 opt2");
+	printf("# Read: o_char_3 %s\n", o_char_3);
 	assert(strcmp(o_char_3, ((argc > 8 ? argv[8] : "opt1 opt2"))) == 0);
 
 	timer t1 = create_timer("timer_1");
 	int r_int_1 = create_reportable_int("r_int_1", argc);
+	printf("# Report: r_int_1 %d\n", r_int_1);
 	assert(r_int_1 == argc);
 
 	double r_double_1 = create_reportable_double("r_double_1", v_double_1 / o_double_1);
+	printf("# Report: r_double_1 %lf\n", r_double_1);
 	assert(r_double_1 == v_double_1 / o_double_1);
 
 	char * r_char_1 = create_reportable_char_p("r_char_1", "hello");
+	printf("# Report: r_char_1 %s\n", r_char_1);
 	assert(strcmp(r_char_1, "hello") == 0);
 
 	char * r_char_2 = create_reportable_char_p("r_char_2", "hello world");
+	printf("# Report: r_char_2 %s\n", r_char_2);
 	assert(strcmp(r_char_2, "hello world") == 0);
 
 	timer t2 = create_timer("timer_2"); // New timer to trigger realloc
+	printf("# Start sleep\n");
 	sleep(1);
+	printf("# End sleep\n");
 	stop_timer(&t2);
 
-	const double diff_time = getNS_timer(&t2);
-	assert(getNS_timer(&t2) > 1E9);
+	const double diff_time1 = getNS_timer(&t1);
+	printf("# Timer: T1 %lf\n", diff_time1);
+	assert(diff_time1 == 0);
 
-	assert(getNS_timer(&t1) == 0);
+	const double diff_time2 = getNS_timer(&t2);
+	printf("# Times: T2 %lf\n", diff_time2);
+	assert(diff_time2 >= 1E9);
 
+	printf("# Reporting args\n");
 	report_args();
 
 	report_args_json();
 
+	printf("\n# Call free args\n");
 	free_args ();
 	return 0;
 }
